976436709ee9dc111eacd012d2c0c7ffef9f80ea
pradeep2512m <pradeepm6383@gmail.com>
2022-03-03 10:39:20 +0530

    reverse

---
+++ reverse.py
@@ -0,0 +1,7 @@
+a = int(input("25122:"))
+reverse = 0
+while(a>0):
+    remainer = a % 10
+    reverse = (reverse*10) + remainder
+    nnumber = a // 10
+    print("\n reverse of entered number is = %d"%d reverse)
